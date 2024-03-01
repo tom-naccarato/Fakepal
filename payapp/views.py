6f1972080585d6a5a604b9f4caa8088d1002f3dd
tom-naccarato <tcarlnaccarato@gmail.com>
2024-03-01 22:43:29 +0000

    Pluralise transaction count and format created_at

--- payapp/views.py
+++ payapp/views.py
@@ -3,7 +3,7 @@ from django.contrib.auth.decorators import user_passes_test
 from django.db.models import Q
 from django.shortcuts import render, redirect
 
-from payapp.models import Transaction
+from payapp.models import Transaction, Account
 from webapps2024 import settings
 
 
@@ -11,6 +11,7 @@ def login_required_message(function):
     """
     Decorator to display a message if the user is not logged in
     """
+
     def wrap(request, *args, **kwargs):
         # If the user is logged in, call the function
         if request.user.is_authenticated:
@@ -19,15 +20,32 @@ def login_required_message(function):
         else:
             messages.error(request, "You need to be logged in to view this page.")
             return redirect('%s?next=%s' % (settings.LOGIN_URL, request.path))
+
     wrap.__doc__ = function.__doc__
     wrap.__name__ = function.__name__
     return wrap
 
+
 def home(request):
     return render(request, 'payapp/home.html')
 
+
 @login_required_message
 def transactions(request):
-    transactions_list = (Transaction.objects.filter(Q(sender__user=request.user) | Q(receiver__user=request.user))
-                         .select_related('sender', 'receiver'))
+    """
+    View function to display the transactions of the logged-in user
+
+    :param request:
+    :return:
+    """
+    # Create a test transaction to see if the query works
+    transaction = Transaction.objects.create(
+        sender=Account.objects.get(user=request.user),
+        receiver=Account.objects.get(user=request.user),
+        amount=100
+    )
+    transactions_list = Transaction.objects.filter(
+        Q(sender__user=request.user) | Q(receiver__user=request.user)
+    ).select_related('sender', 'receiver', 'sender__user', 'receiver__user')
+    print(transactions_list)
     return render(request, 'payapp/transactions.html', {'transactions': transactions_list})
