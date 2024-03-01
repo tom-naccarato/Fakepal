97f140be5886a3f795fc7aef339a0198ee2ae6f8
tom-naccarato <tcarlnaccarato@gmail.com>
2024-03-01 20:06:41 +0000

    Add docstring for UserForm

--- register/forms.py
+++ register/forms.py
@@ -2,7 +2,11 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from payapp.models import Account
 
+
 class UserForm(UserCreationForm):
+    """
+    Form for creating a new user and account
+    """
     class Meta:
         model = User
         fields = ['username', 'email', 'password1', 'password2', 'first_name', 'last_name']
@@ -23,4 +27,3 @@ class UserForm(UserCreationForm):
             account = Account(user=user)
             account.save()
         return user
-
