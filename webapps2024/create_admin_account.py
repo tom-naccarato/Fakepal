b5a83b8b734010d413f44013b3bfd766058dc574
tom-naccarato <tcarlnaccarato@gmail.com>
2024-03-02 16:22:32 +0000

    Add create admin group

---
+++ webapps2024/create_admin_account.py
@@ -0,0 +1,46 @@
+from django.contrib.auth.models import Group, Permission, User
+from django.contrib.contenttypes.models import ContentType
+
+def create_admin_group():
+    """Function to create a custom admin group and assign all permissions to it."""
+    # Create the admin group
+    admin_group, created = Group.objects.get_or_create(name='AdminGroup')
+
+    # Get or create permissions
+    permissions = Permission.objects.all()
+
+    # Add the permissions to the group
+    admin_group.permissions.set(permissions)
+
+    # If the group was created, print a message otherwise print a different message
+    if created:
+        print('Custom admin group created and permissions assigned.')
+    else:
+        print('Custom admin group already exists.')
+
+def create_admin_account():
+    """Function to create an admin account as per specification."""
+    # Check if the admin account already exists
+    if not User.objects.filter(username='admin1').exists():
+        # Create the admin account
+        admin = User.objects.create_user('admin1', password='admin1', email="admin1@email.com")
+
+        # Add the admin to the admin group
+        admin_group = Group.objects.get(name='AdminGroup')
+        admin.groups.add(admin_group)
+
+        # Print a message to confirm the account was created
+        print('Admin account created.')
+    else:
+        # Print a message to confirm the account already exists
+        print('Admin account already exists.')
+
+
+def create_admin_group_and_account():
+    """Function to create the admin group and account."""
+    create_admin_group()
+    create_admin_account()
+    print('Admin group and account created successfully.')
+
+
+
--- webapps2024/wsgi.py
+++ webapps2024/wsgi.py
@@ -10,7 +10,13 @@ https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
+from create_admin_account import create_admin_group_and_account
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'webapps2024.settings')
 
 application = get_wsgi_application()
+
+# Creates the admin account and group on startup
+
+create_admin_group_and_account()
+
