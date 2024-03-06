5f6b028552bb778f056aba01c71d1d44c5b4064d
tom-naccarato <tcarlnaccarato@gmail.com>
2024-03-06 18:39:06 +0000

    Add comments to tag

--- register/templatetags/group_tags.py
+++ register/templatetags/group_tags.py
@@ -5,10 +5,20 @@ from django.contrib.auth.models import Group
 register = template.Library()
 
 
+# Registers the is_admin filter
 @register.filter(name="is_admin")
+# Defines the is_admin filter
 def is_admin(user):
+    """
+    Checks if the user is in the AdminGroup
+    :param user:
+    :return:
+    """
+    # Tries to get the AdminGroup
     try:
         group = Group.objects.get(name='AdminGroup')
+    # If the AdminGroup does not exist, returns False
     except Group.DoesNotExist:
         return False
+    # Returns True if the user is in the AdminGroup, False otherwise
     return group in user.groups.all()
