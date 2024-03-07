e436f213bf6cf351ffa9c6a1c546ae0f90974386
tom-naccarato <tcarlnaccarato@gmail.com>
2024-03-07 04:19:31 +0000

    Add badge icon for incoming requests

---
+++ payapp/context_processors.py
@@ -0,0 +1,20 @@
+from django.shortcuts import get_object_or_404
+
+from payapp.models import Request, Account
+
+
+
+def incoming_requests_count(request):
+    """
+    This function returns the number of incoming transactions for the logged-in user
+    :param request: HttpRequest object
+    :return: Dictionary containing the count of incoming requests
+    """
+    incoming_count = None  # Default value
+
+    if request.user.is_authenticated:
+        user_account = get_object_or_404(Account, user=request.user)
+        incoming_count = Request.objects.filter(receiver=user_account, status='pending').count()
+        return {'incoming_requests_count': incoming_count}
+    else:
+        return {'incoming_requests_count': None}
