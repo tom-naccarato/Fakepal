bb23e50c4c80dac337bdb85712286927c883e538
tom-naccarato <tcarlnaccarato@gmail.com>
2024-03-08 21:04:50 +0000

    Update custom_exceptions.py

--- payapp/custom_exceptions.py
+++ payapp/custom_exceptions.py
@@ -2,6 +2,15 @@ class InsufficientBalanceException(Exception):
     """
     Exception raised when a user tries to accept a request but does not have enough balance.
     """
+
     def __init__(self, message="Insufficient balance to complete the transaction"):
+        self.message = message
+        super().__init__(self.message)
+
+
+class CurrencyConversionError(Exception):
+    """Exception raised for errors in the currency conversion process."""
+
+    def __init__(self, message="Error in currency conversion, please try again"):
         self.message = message
         super().__init__(self.message)
