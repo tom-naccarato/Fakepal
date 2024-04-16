d0d35bf185551a772619ad09c8d78887ffd01429
tom-naccarato <tcarlnaccarato@gmail.com>
2024-04-16 01:14:22 +0100

    Update allowed_hosts

--- webapps2024/settings.py
+++ webapps2024/settings.py
@@ -28,7 +28,7 @@ RUNSERVERPLUS_SERVER_ADDRESS_PORT = '0.0.0.0:8000'
 
 ALLOWED_HOSTS = [
     '52.203.137.55',  # AWS
-    'localhost'  # localhost
+    '127.0.0.1',  # localhost
 ]
 
 # Application definition
