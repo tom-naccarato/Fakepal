d1291236c3c5830629adbd07e87a88a04c3a43ab
tom-naccarato <tcarlnaccarato@gmail.com>
2024-05-01 06:07:59 +0100

    Add base_url variable

--- webapps2024/settings.py
+++ webapps2024/settings.py
@@ -143,3 +143,5 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+BASE_URL = 'https://localhost:8000'
